c141e6ea4bfc0ae7bb9818aa3d967b3374b9ff36
Kalki Eshwar D <77596443+KalkiEshwarD@users.noreply.github.com>
2023-01-15 15:18:24 +0530

    Update ex01.py
    Signed-off-by: Kalki Eshwar D <77596443+KalkiEshwarD@users.noreply.github.com>

--- PPS2/ex01.py
+++ PPS2/ex01.py
@@ -3,6 +3,13 @@
 # If list object is not present in the existing list, it should say “cannot unpack”.
 
 def list_unpacker(list0):
+    """
+    INPUT:      list0 [list]
+    OUTPUT:     total_objects / cannot unpack [integer / string]
+
+    DESCRIPTION:    This function takes in a list as input upacks all the elements inside list objects in the input list and then outputs all the elements in the input list.
+    DEPENDENCIES:   Nil
+    """
     total_objects = 0
     list_obj = 0
 
@@ -13,18 +20,18 @@ def list_unpacker(list0):
             list_obj += 1
         else:
             total_objects += 1
-        
-    if list_obj > 0:
-        return total_objects
-    else:
+
+    if list_obj == 0:
         return 'cannot unpack'
+    else:
+        return total_objects
 
 
 def main():
     input_stuff = eval(input())
     result = list_unpacker(input_stuff)
     print(result)
-
+    
 
 if __name__ == "__main__":
     main()
