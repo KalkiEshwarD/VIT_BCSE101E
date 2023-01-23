0562fd79744692ec5535301afd4bf0d175b1b49a
Kalki Eshwar D <77596443+KalkiEshwarD@users.noreply.github.com>
2023-01-23 10:59:43 +0530

    Create ex17.py
    Signed-off-by: Kalki Eshwar D <77596443+KalkiEshwarD@users.noreply.github.com>

---
+++ Mock_FAT/ex17.py
@@ -0,0 +1,16 @@
+# WAP to check whether two input string are isomorphic or not.
+
+# Two strings str1 and str2 are called isomorphic if there is a one-to-one mapping possible for every character of str1 to every character of str2. And all occurrences of every character in ‘str1’ map to the same character in ‘str2’.
+
+# Sample input:
+# aab
+# xxy
+# Sample output:
+# True
+
+# Sample input:
+# aab
+# xyz
+# Sample output
+# False
+
