1aea0d24e814f5c47f4a87b5447f54eefba505cc
Kalki Eshwar D <77596443+KalkiEshwarD@users.noreply.github.com>
2023-01-23 11:14:09 +0530

    Create ex21.py
    Signed-off-by: Kalki Eshwar D <77596443+KalkiEshwarD@users.noreply.github.com>

---
+++ Mock_FAT/ex21.py
@@ -0,0 +1,10 @@
+# Find the total number of non-empty substrings of an input  string.
+
+# Input : abc 
+# Output : 6 
+# Explanation Every substring of the given string : “a”, “b”, “c”, “ab”, “bc”, “abc”
+
+# Input : abcd 
+# Output : 10 
+# Explanation Every substring of the given string : “a”, “b”, “c”, “d”, “ab”, “bc”, “cd”, “abc”, “bcd” and “abcd”
+
