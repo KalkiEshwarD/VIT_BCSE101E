33ae131b4999eebd88dd97224e0f4bd3f2d494d2
Kalki Eshwar D <77596443+KalkiEshwarD@users.noreply.github.com>
2023-01-23 11:53:55 +0530

    Update ex21.py
    Signed-off-by: Kalki Eshwar D <77596443+KalkiEshwarD@users.noreply.github.com>

--- Mock_FAT/ex21.py
+++ Mock_FAT/ex21.py
@@ -8,3 +8,29 @@
 # Output : 10 
 # Explanation Every substring of the given string : “a”, “b”, “c”, “d”, “ab”, “bc”, “cd”, “abc”, “bcd” and “abcd”
 
+def subs_finder(string0):
+    
+    num_subs = 0
+    
+    for i in range(1, len(string0) + 1):
+        
+        subs_list = []
+        for index in range(len(string0)):
+            if index + i <= len(string0):
+                try:
+                    subs_list.append(string0[index: index + i])       
+                except:
+                    pass
+        num_subs += len(subs_list)
+    
+    return num_subs
+
+def main():
+    input_stuff = input()
+    result = subs_finder(input_stuff)
+    print(result)
+
+
+if __name__ == "__main__":
+    main()
+    
