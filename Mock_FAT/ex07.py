ae2be8b034fa26b396acc0b85249da2d4ef01a00
Kalki Eshwar D <77596443+KalkiEshwarD@users.noreply.github.com>
2023-01-23 12:24:48 +0530

    Update ex07.py
    Signed-off-by: Kalki Eshwar D <77596443+KalkiEshwarD@users.noreply.github.com>

--- Mock_FAT/ex07.py
+++ Mock_FAT/ex07.py
@@ -6,3 +6,30 @@
 # Sample output
 # (7, 2), (9, 1), (-2, 1), (-7, 1)
 
+def freq_plotter(list0):
+    freq_dict = {}
+    for integer in list0:
+        if integer in freq_dict.keys():
+            freq = freq_dict[integer]
+            freq_dict[integer] = freq + 1 
+        else:
+            freq_dict[integer] = 1
+    
+    
+    freq_list = list(set{list(freq_dict.values())})
+    
+    for freq in freq_list:
+        keys = [dict_keys for dict_keys, dict_values in freq_dict.items()]
+        
+    return freq_dict
+    
+
+def main():
+    input_stuff = list(eval(input()))
+    result = freq_plotter(input_stuff)
+    print(result)
+
+
+if __name__ == "__main__":
+    main()
+    
