1f0f46dbbfc299638142b5a855c498bba9aa01ff
Kalki Eshwar D <77596443+KalkiEshwarD@users.noreply.github.com>
2023-01-23 10:45:59 +0530

    Create ex07.py
    Signed-off-by: Kalki Eshwar D <77596443+KalkiEshwarD@users.noreply.github.com>

---
+++ Mock_FAT/ex07.py
@@ -0,0 +1,8 @@
+# Write program to input a list and print the frequency of unique numbers in sorted order of the frequency and the integer value in reverse order.
+
+# Sample input
+# 7, 7, -2, -7, 9
+
+# Sample output
+# (7, 2), (9, 1), (-2, 1), (-7, 1)
+
