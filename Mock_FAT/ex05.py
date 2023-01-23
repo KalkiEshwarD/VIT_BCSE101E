9389e14f0edf0a4524fc93dda6f091c330c6bd6f
Kalki Eshwar D <77596443+KalkiEshwarD@users.noreply.github.com>
2023-01-23 10:44:36 +0530

    Create ex05.py
    Signed-off-by: Kalki Eshwar D <77596443+KalkiEshwarD@users.noreply.github.com>

---
+++ Mock_FAT/ex05.py
@@ -0,0 +1,17 @@
+# Write a Function (WAF) to find roots of a quadratic equation of the form: $ax^2 + bx +c$. Assume a,b,c to be integers.
+# a)print the appropriate statements based o
+#            i) roots are real and different roots
+#           ii) real and same roots
+#          iii) Complex Roots
+# b)  Write the roots in ascending order and upto two decimal places.
+
+# Note: You may use math module for finding square root.
+# Sample input:
+# 1
+# -5
+# 6
+# Sample Output
+# real and different roots
+# -3.0
+# -2.0
+
