0da5c0b1fd72064cc2ff9c516eba396960f89f4a
Kalki Eshwar D <77596443+KalkiEshwarD@users.noreply.github.com>
2023-01-23 10:58:57 +0530

    Create ex16.py
    Signed-off-by: Kalki Eshwar D <77596443+KalkiEshwarD@users.noreply.github.com>

---
+++ Mock_FAT/ex16.py
@@ -0,0 +1,11 @@
+# WAP to take an i/p string and find its length of string using inbuilt function.
+
+def main():
+    input_stuff = input()
+    result = len(input_stuff)
+    print(result)
+
+
+if __name__ == "__main__":
+    main()
+    
