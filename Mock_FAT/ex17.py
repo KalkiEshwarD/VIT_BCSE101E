dc36eaadf94699e5f1d9703450a12a86b1503f80
Kalki Eshwar D <77596443+KalkiEshwarD@users.noreply.github.com>
2023-01-24 20:35:00 +0530

    Update ex17.py
    Signed-off-by: Kalki Eshwar D <77596443+KalkiEshwarD@users.noreply.github.com>

--- Mock_FAT/ex17.py
+++ Mock_FAT/ex17.py
@@ -12,45 +12,31 @@
 # aab
 # xyz
 # Sample output
-# False
 
-# Write program to input a list and print the frequency of unique numbers in sorted order of the frequency and the integer value in reverse order.
+def isomorphic_checker(string1, string2):
+    corr_char_dict = {}
 
-# Sample input
-# 7, 7, -2, -7, 9
+    if range(len(string1)) != range(len(string2)):
+        return False
 
-# Sample output
-# (7, 2), (9, 1), (-2, 1), (-7, 1)
-
-def freq_counter(list0):
-    freq_dict = {}
-    for integer in list0:
-        if integer in freq_dict.keys():
-            freq = freq_dict[integer]
-            freq_dict[integer] = freq + 1
+    for char_index in range(len(string1)):
+        if string1[char_index] in corr_char_dict.keys():
+            if string2[char_index] != corr_char_dict[string1[char_index]]:
+                return False
         else:
-            freq_dict[integer] = 1
-
-    return freq_dict
-
-
-def freq_sorter(freq_dict0):
-    freq_list = []
-    for key, value in freq_dict0.items():
-        # print(key, value)
-        freq_list.append([key, value])
-    # print(freq_list)
-    freq_list.sort(key=lambda x: (x[1], x[0]), reverse=True)
-    # freq_list.sort(key=lambda x: x[0], reverse=True)
-    # print(freq_list)
-    return freq_list
-
-
+            corr_char_dict[string1[char_index]] = string2[char_index]
+            
+    return True
 
 def main():
-    # input_stuff = list(eval(input()))
-    input_stuff = list(eval('7, 7, -2, -7, 9'))
-    result = freq_sorter(freq_counter(input_stuff))
+    # input_stuff1 = input()
+    # input_stuff2 = input()
+    input_stuff1 = 'aab'
+    input_stuff2 = 'xxy'
+    if isomorphic_checker(input_stuff1, input_stuff2) and isomorphic_checker(input_stuff2, input_stuff1):
+        result = True
+    else:
+        result = False
     print(result)
 
 
