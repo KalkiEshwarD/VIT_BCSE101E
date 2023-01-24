248c1a2e1e529896479dd3698f090a5546de2a20
Kalki Eshwar D <77596443+KalkiEshwarD@users.noreply.github.com>
2023-01-24 20:34:19 +0530

    Update ex07.py
    Signed-off-by: Kalki Eshwar D <77596443+KalkiEshwarD@users.noreply.github.com>

--- Mock_FAT/ex07.py
+++ Mock_FAT/ex07.py
@@ -6,30 +6,37 @@
 # Sample output
 # (7, 2), (9, 1), (-2, 1), (-7, 1)
 
-def freq_plotter(list0):
+def freq_counter(list0):
     freq_dict = {}
     for integer in list0:
         if integer in freq_dict.keys():
             freq = freq_dict[integer]
-            freq_dict[integer] = freq + 1 
+            freq_dict[integer] = freq + 1
         else:
             freq_dict[integer] = 1
-    
-    
-    freq_list = list(set{list(freq_dict.values())})
-    
-    for freq in freq_list:
-        keys = [dict_keys for dict_keys, dict_values in freq_dict.items()]
-        
+
     return freq_dict
-    
+
+
+def freq_sorter(freq_dict0):
+    freq_list = []
+    for key, value in freq_dict0.items():
+        # print(key, value)
+        freq_list.append([key, value])
+    # print(freq_list)
+    freq_list.sort(key=lambda x: (x[1], x[0]), reverse=True)
+    # freq_list.sort(key=lambda x: x[0], reverse=True)
+    # print(freq_list)
+    return freq_list
+
+
 
 def main():
-    input_stuff = list(eval(input()))
-    result = freq_plotter(input_stuff)
+    # input_stuff = list(eval(input()))
+    input_stuff = list(eval('7, 7, -2, -7, 9'))
+    result = freq_sorter(freq_counter(input_stuff))
     print(result)
 
 
 if __name__ == "__main__":
     main()
-    
